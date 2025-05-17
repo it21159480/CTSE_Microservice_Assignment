f46c2619509855008386d600750a680c91b64e75
Mufeel Ishad <99629231+it21159480@users.noreply.github.com>
2025-05-17 22:06:51 +0530

    docker implementation added

---
+++ backend/auth-microservice/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.js';
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+app.use('/api', authRoutes);
+
+app.get('/health', (req, res) => res.status(200).send('Service is runnig!'));
+
+// Only start server if not in test environment
+if (process.env.NODE_ENV !== 'test') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+export default app;
